52cdebcc40dffb8d04427bf2e358ebc5c3a631dc
RomaLavrinenko <73121041+RomaLavrinenko@users.noreply.github.com>
2023-06-16 17:15:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// let a = prompt('Write number 1-12');
+
+// switch(a){
+//     case '1':
+//     case '2':
+//     case '12':
+//         alert("winter");
+//         break;
+//     case '3':
+//     case '4':
+//     case '5':
+//         alert("spring");
+//         break;
+//     case '6':
+//     case '7':
+//     case '8':
+//         alert("summer");
+//         break;
+//     case '9':
+//     case '10':
+//     case '11':
+//         alert("autumn");
+//         break;
+//     default:
+//         alert("You write wrong number");
+//         break;
+// }
+
+
+
+// function naturalNumb(n){
+// nextPrime:
+// for (let i = 2; i <= n; i++) { 
+
+//   for (let j = 2; j < i; j++) { 
+//     if (i % j == 0) continue nextPrime; 
+//   }
+//   console.log(i);
+// }
+// }
+// naturalNumb(100);
+ 
+// function maxNumber(){
+//     console.log(arguments);
+//     for(let c = 0;c<=arguments.length;c++){
+//         for(let i = 0;i <= arguments.length;i++){
+//             if(arguments[c]>arguments[i]-1){
+//                 console.log(`max number 7`)
+//             }
+
+//         }
+//     }
+// }
+// maxNumber(5,7,3);
